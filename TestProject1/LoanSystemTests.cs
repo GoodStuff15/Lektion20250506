942fe63846d5845078016f218623d50c5a439ee9
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-05-06 12:22:04 +0200

    Testing testing

--- TestProject1/LoanSystemTests.cs
+++ TestProject1/LoanSystemTests.cs
@@ -3,8 +3,38 @@ namespace TestProject1;
 [TestClass]
 public class LoanSystemTests
 {
+    // Register new loans
     [TestMethod]
-    public void AddB()
+    public void RegisterLoan_MarkBookAsLoanedOut()
     {
+    }
+    
+    [TestMethod]
+    public void RegisterLoan_CannotRegisterLoanOfBookThatIsLoanedOut()
+    {
+    }
+
+    [TestMethod]
+    public void RegisterLoan_SetCorrectDateWhenLoaningOut()
+    {
+
+    }
+
+    // Returning loans
+
+    [TestMethod]
+    public void ReturnLoan_CanBookBeReturned()
+    {
+
+    }
+    [TestMethod]
+    public void ReturnLoan_IsBookLoanDateReset()
+    {
+
+    }
+    [TestMethod] 
+    public void ReturnLoan_IsBookLoanedOutWhenReturning()
+    { 
+    
     }
 }
